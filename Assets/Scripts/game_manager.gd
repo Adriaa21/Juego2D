593b589dde97b56844787df334877bb6881060e0
Adria21 <adriaperis99@gmail.com>
2026-06-10 12:56:46 +0200

    Juego2D

---
+++ Assets/Scripts/game_manager.gd
@@ -0,0 +1,46 @@
+extends Node
+
+var starting_area = 1
+var current_area = 1
+var area_path = "res://Assets/Scenes/Areas/"
+
+var energy_cells = 0
+var area_container : Node2D
+var player : PlayerController
+
+func _ready():
+	area_container = get_tree().get_first_node_in_group("area_container")
+	player = get_tree().get_first_node_in_group("player")
+	load_area(starting_area)
+	
+	reset_energy_cells()
+
+func next_area():
+	current_area += 1
+	load_area(current_area)
+	
+func load_area(area_number):
+	#chech scene path
+	var full_path = area_path + "area_" + str(current_area) + ".tscn"
+	#get_tree().change_scene_to_file(full_path)
+	#print("The player has moved to area " + str(current_area))
+	var scene = load(full_path) as PackedScene
+	if !scene:
+		return
+	#remove the previuos scene
+	for child in area_container.get_children():
+		child.queue_free()
+		await child.tree_exited
+	var instance = scene.instantiate()
+	area_container.add_child(instance)
+	reset_energy_cells()
+	var player_start_position = get_tree().get_first_node_in_group("player_start_position") as Node2D
+	player.teleport_to_location(player_start_position.position)
+func add_energy_cell():
+	energy_cells += 1
+	if energy_cells >= 4:
+		var portal = get_tree().get_first_node_in_group("area_exits") as AreaExit
+		portal.open()
+
+func reset_energy_cells():
+	energy_cells = 0
