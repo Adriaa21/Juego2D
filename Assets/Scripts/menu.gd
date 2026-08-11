f21511336844780e2c9c0b861606e393c6807682
Adria21 <adriaperis99@gmail.com>
2026-08-11 22:00:44 +0200

    Intento de optimizar codigo 1

--- Assets/Scripts/menu.gd
+++ Assets/Scripts/menu.gd
@@ -1,12 +1,11 @@
 extends Control
 
 
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Assets/Scenes/UI/gameplay.tscn")
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _on_exit_pressed() -> void:
--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -6,26 +6,22 @@ class_name PlayerController
 
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
-var direction = 0
+var direction = 0.0
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _input(event):
-	# Handle jump.
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_power * jump_multiplier
-	# Handle jump down
+
 	if event.is_action_pressed("move_down"):
 		set_collision_mask_value(10, false)
 	else:
 		set_collision_mask_value(10, true)
 
 func _physics_process(delta):
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
